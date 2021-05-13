4b2925afe85563c19d8114384a19328c64f2ac89
dongjinBaek <dj9136@gmail.com>
2021-05-13 07:26:09 +0000

    add simple map to index.jsp

--- src/main/java/project/map/MapController.java
+++ src/main/java/project/map/MapController.java
@@ -4,11 +4,18 @@ import org.springframework.stereotype.Controller;
 //import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 
 @Controller
 public class MapController {
+    @Autowired
+    Environment env;
+    
     @GetMapping("/")
     public ModelAndView index() {
-        return new ModelAndView("index");
+        ModelAndView mav = new ModelAndView("index");
+        mav.addObject("appKey", env.getProperty("app.key"));
+        return mav;
     }
 }
