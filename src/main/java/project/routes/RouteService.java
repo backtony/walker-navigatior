d9c8a0b2d4cee046277deb3c1d69163784b977c7
Dongjin Baek <dj9136@gmail.com>
2021-05-14 01:15:52 +0900

    handled case when endY < startY

--- src/main/java/project/routes/RouteService.java
+++ src/main/java/project/routes/RouteService.java
@@ -9,6 +9,7 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import project.dao.AccidentDAO;
+import project.dao.CctvDAO;
 import project.dao.LampDAO;
 import project.routes.model.*;
 
@@ -24,6 +25,8 @@ public class RouteService {
     Environment env;
     @Autowired
     AccidentDAO accidentDAO;
+    @Autowired
+    CctvDAO cctvDAO;
 
     public PedestrianApiResponse getResponse(Double startX, Double startY, Double endX, Double endY, Double wayX,
                                              Double wayY) {
@@ -46,17 +49,26 @@ public class RouteService {
     }
 
     public List<Candidate> findCandidates(double startX, double startY, double endX, double endY) {
+        double llX = Math.min(startX, endX), llY = Math.min(startY, endY),
+                urX=Math.max(startX, endX), urY=Math.max(startY, endY);
         PointBox pb = new PointBox(startX, startY, endX, endY);
         List<Candidate> bestCandidates = Arrays.asList(new Candidate[5]);
-        List<Accident> accidents = accidentDAO.getAccidentsInRange(startX, startY, endX, endY);
+        List<Accident> accidents = accidentDAO.getAccidentsInRange(llX, llY, urX, urY);
         Accident[] accidentArr = new Accident[accidents.size()];
         for (int i=0; i<accidents.size(); i++) {
             accidentArr[i] = accidents.get(i);
         }
+        List<Location> cctvs = cctvDAO.getCctvsInRange(llX, llY, urX, urY);
+        Location[] cctvArr = new Location[cctvs.size()];
+        for (int i=0; i<cctvs.size(); i++) {
+            cctvArr[i] = cctvs.get(i);
+        }
         Candidate noWayCand = new Candidate();
+        noWayCand.setAccidents(accidentArr);
         noWayCand.setRoutes(getResponse(startX, startY, endX, endY, null, null));
+        noWayCand.calculateCost();
         bestCandidates.set(0, noWayCand);
-        for (Location lamp : LampDAO.getLamps(startX, startY, endX, endY)) {
+        for (Location lamp : LampDAO.getLamps(llX, llY, urX, urY)) {
             int res = pb.findSector(lamp.getX(), lamp.getY());
             if (res == 0) continue;
             Candidate cand = new Candidate();
@@ -66,6 +78,11 @@ public class RouteService {
             double cost = cand.getCost();
             if (bestCandidates.get(res) == null || cost < bestCandidates.get(res).getCost()) {
                 bestCandidates.set(res, cand);
+            }
+        }
+        for (Candidate cand : bestCandidates) {
+            if (cand != null) {
+                cand.setCctvs(cctvArr);
             }
         }
         return bestCandidates;
