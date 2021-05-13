9afab9a2acf58ba4e415927a15415487e4c18317
Min-Uk.Lee <makerdark98@gmail.com>
2021-05-13 07:09:21 +0100

    Add jsp configuration

---
+++ src/main/java/project/map/MapController.java
@@ -0,0 +1,14 @@
+package project.map;
+
+import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MapController {
+    @GetMapping("/")
+    public ModelAndView index() {
+        return new ModelAndView("index");
+    }
+}
