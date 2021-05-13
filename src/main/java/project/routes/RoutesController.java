8e47d27be991b70594960a7e8909c6c951ad4fed
Dongjin Baek <dj9136@gmail.com>
2021-05-13 18:01:17 +0900

    Add simle request

---
+++ src/main/java/project/routes/RoutesController.java
@@ -0,0 +1,59 @@
+package project.routes;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+@RequestMapping("/routes")
+@RequiredArgsConstructor
+public class RoutesController {
+
+    @Autowired
+    Environment env;
+
+    @ModelAttribute("appKey")
+    String appKey() {
+        return env.getProperty("app.key");
+    }
+
+    @GetMapping("/")
+    public String path(Model model,
+                       @RequestParam String startX,
+                       @RequestParam String startY,
+                       @RequestParam String endX,
+                       @RequestParam String endY) {
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("appKey", env.getProperty("app.key"));
+
+            String url = "https://apis.openapi.sk.com/tmap/routes/pedestrian";
+            MultiValueMap<String, String> map= new LinkedMultiValueMap<>();
+            map.add("startY", startY);
+            map.add("startX", startX);
+            map.add("endY", endY);
+            map.add("endX", endX);
+            map.add("startName", "start");
+            map.add("endName", "end");
+            HttpEntity<MultiValueMap> entity = new HttpEntity<>(map, headers);
+            Object res = restTemplate.postForObject(url,entity , Object.class);
+            System.out.println(res);
+            model.addAttribute("res", res);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return "index";
+    }
+}
