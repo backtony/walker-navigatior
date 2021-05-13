b4a1f2ff4fa8cfc5d7b288a2e9b9c9f954a848ff
jooomOut <rimeilo324@naver.com>
2021-05-14 00:43:56 +0900

    Add cctv domain
    add CctvDAO, Mapper

---
+++ src/main/java/project/dao/CctvDAO.java
@@ -0,0 +1,21 @@
+package project.dao;
+
+import org.apache.ibatis.annotations.Param;
+import project.routes.model.Location;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface CctvDAO {
+    public static List<Location> getLamps(double x1, double y1, double x2, double y2) {
+        List<Location> dummy = new ArrayList<>();
+        dummy.add(new Location(127.125, 37.51));
+        return dummy;
+    };
+    List<Location> getCctvsInRange(
+            @Param("x1") double x1,
+            @Param("y1") double y1,
+            @Param("x2") double x2,
+            @Param("y2") double y2
+    );
+}
