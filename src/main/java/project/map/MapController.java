b2a90d0022fd86be10491ab94166d782e436a870
Min-Uk.Lee <makerdark98@gmail.com>
2021-05-13 09:27:38 +0100

    Add logger and property util

--- src/main/java/project/map/MapController.java
+++ src/main/java/project/map/MapController.java
@@ -1,10 +1,12 @@
 package project.map;
 
 import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+import lombok.extern.slf4j.Slf4j;
+import project.util.PropertyUtil;
 
+@Slf4j
 @Controller
 public class MapController {
     @GetMapping("/")
