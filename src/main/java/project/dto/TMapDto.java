3e304b288711f67854a7136fc644808288cad1c6
Yunjeong Choi <yunjeongchoi@Yunjeongui-MacBookAir.local>
2021-05-14 00:40:29 +0900

    Tmap Controller

---
+++ src/main/java/project/dto/TMapDto.java
@@ -0,0 +1,44 @@
+package com.example.jsontototototototo.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TMapDto {
+    private SearchPoiInfo searchPoiInfo;
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    //@JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class SearchPoiInfo{
+        private Pois pois;
+    }
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Pois{
+        private List<Poi> poi;
+    }
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Poi{
+        private String name;
+        private NewAddressList newAddressList;
+    }
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class NewAddressList{
+        private List<NewAddress> newAddress;
+    }
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class NewAddress{
+        private String centerLat;
+        private String centerLon;
+    }
+}
